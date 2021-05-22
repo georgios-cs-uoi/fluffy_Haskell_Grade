6912dc6076ce9027e769b7f6b9003944642ef5bb
georgios-cs-uoi <58822255+georgios-cs-uoi@users.noreply.github.com>
2021-05-22 20:43:56 +0300

    Update Grades.hs

--- Grades.hs
+++ Grades.hs
@@ -5,20 +5,3 @@ grade a b
 	| (c<50 && c>47 && a>47) = 50
 	| otherwise = c
 	where c = (div (8*a) 10)+b
-
-countDigits :: Int -> Int -> Int
-countDigits 0 0 = 0
-countDigits a b = countDigits (div a 10) (div b 10) + if (mod a 10)==(mod b 10) then 1 else 0
-
-digits :: Int -> Int -> Int
-digits  a b 
-	|n == 8 = 1000000
-	|n == 7 = 100000
-	|n == 6 = 8000
-	|n == 5 = 300
-	|n == 4 = 20
-	|n == 3 = 5
-	|n == 2 = 1
-	|otherwise = 0
-		where n = countDigits a b
-
